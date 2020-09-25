50845fc7d00c23d0f88572cab713a8b6f51a4dea
HunterS. DiCicco <hsdicicco@gmail.com>
2020-09-25 12:38:00 -0400

    `click` boilerplater, sussing out CLI's desired behavior and writing helplines in advance

---
+++ cli/sfake.py
@@ -0,0 +1,66 @@
+import click
+
+import pandas as pd
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+import feature_extraction as fe
+
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
+#DATA_TAXONOMY = pd.read_csv('data/d_full.csv')
+
+try:
+    with open('data/texts_labelled.pickle', mode='rb') as filein:
+        TRAIN_TEXTS = pickle.load(filein)
+except:
+    # download from hosted source
+    pass
+
+@click.option('--tfidf_kwargs',
+              default={},
+              short_help='Arguments for `sklearn.feature_extraction.text.TfidfVectorizer`.',
+              help="Arguments in the form of a `dict`, \
+                    to be passed to the `TfidfVectorizer`. \
+                    Passing these arguments lets you configure \
+                    pre-processing and define whether you want \
+                    the transformer to make n-grams., \
+                    among other options.")
+def tfidf_transform(single_text, **tfidf_kwargs):
+    return TfidfVectorizer(**tfidf_kwargs).fit_transform(TRAIN_TEXTS + [single_text])
+
+@click.command('predict',
+               context_settings=CONTEXT_SETTINGS,
+               short_help='Classify a new sample or set of samples with using a given model.',
+               help="")
+@click.option('--model',
+              default='sgd-taxonomy',
+              short_help='Choice of model to use in prediction from those in the `models` directory.',
+              help="The filename of the model of choice \
+                    to predict the new sample. Can be any \
+                    `.pickle` file in the `models` directory. \
+                    By default we use a log loss stochastic gradient descent\
+                    model trained on the fake news taxonomy features from \
+                    *Fake News Is Not Simply False Information, Explication & Taxonomy*")
+@click.option('--text-set',
+              short_help='Path to a `.csv` file containing a series of texts to be classified.')
+@click.option('--single-text',
+              short_help='When flagged, predict a single text provided from \
+                          user input enclosed in double quotes (\"\").')
+@click.argument('--rep',
+                short_help="One of ('bert', 'tfidf', 'taxonomy').",
+                help="Data representation you would like to use to predict the text. \
+                      `taxonomy` (n=14273) is the most complete and well-studied representation. \
+                      Its performance varies between news sources but its associated transformation is very fast. \
+                      `tfidf` (n=654) is robust in most cases but can fail in articles that use idioms or demonstrate uncharacteristic word choices. \
+                      `bert` (n=654, aka Bidirectional Encoder Representation from Transformers) is one of the best text representations, \
+                      with a focus on semantic meaning *in context* with other words within a fixed-length 'attention mask'. \
+                      It requires that text be segmented and run through a large neural network, which may be \
+                      memory-heavy. See the original paper at https://arxiv.org/abs/1810.04805 for more details.")
+def predict(text_set, single_text, model, rep):
+    try:
+        with open(f'models/{model}.pickle', mode='rb') as filein:
+            model_obj = pickle.load(filein)
+    except:
+        raise FileNotFoundError("Model file not found in the `models` directory")
+    
