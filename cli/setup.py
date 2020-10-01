2fbea63c902ae3aa01c68c5610cc76229c7ddae4
HunterS. DiCicco <hsdicicco@gmail.com>
2020-10-01 17:58:34 -0400

    +Boilerplate setup script

---
+++ cli/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="SysFake CLI",
+    version="1.1.0",
+    py_modules=[],
+    include_package_data=True,
+    install_requires=["scikit-learn", "torch"]
+)
