81941f41f402b7f03ed524839cbfa09ba6333b55
HunterS. DiCicco <hsdicicco@gmail.com>
2020-10-02 16:04:54 -0400

    `-R`  to print classification report, outputs go to unique files, `pandas` input validation/NA behavior

--- cli/sfake.py
+++ cli/sfake.py
@@ -1,7 +1,16 @@
+"""
+TODO: pass the pooling strategy as a command line option
+TODO: +classification report at end of prediction pipeline
+TODO: write predictions to new `.csv` or back into the original csv with a `.filter` call 
+"""
 import os
+import pdb
 import glob
 import joblib
+import pathlib
 import warnings
+
+from datetime import datetime as dt
 
 from functools import partial
 
@@ -12,6 +21,7 @@ import torch
 import click
 
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import classification_report
 
 from transformers import BertTokenizer, BertModel
 
@@ -25,10 +35,12 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 # first direction: int -> str
 CLASS_DICT = dict(zip((1, 2, 3, 5, 7, 9),
                       ('real', 'fake', 'opinion', 'polarized', 'satire', 'promotional')))
+INT_TARGETS = tuple(CLASS_DICT.keys())
+STR_TARGETS = tuple(CLASS_DICT.values())
 # next: update the dict with its own inverse for str -> int
 CLASS_DICT.update({v:k for k, v in CLASS_DICT.items()})
 
-REPRESENTATIONS = ('bert', 'tfidf', 'taxonomy')
+REPRESENTATIONS = ('bert', 'tfidf', 'taxonomy', '')
 
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -39,14 +51,13 @@ except:
     # download from hosted source
     pass
 
-click.echo(Figlet(font='larry3d').renderText('sysfake'))
+click.echo(Figlet(font='larry3d').renderText('sysfake').replace('L', '_'))
 click.echo(f"Using device: {DEVICE}")
 
 def bert_transform(single_text, pooling_strategy='cat'):
     """
     Create a BERT embedding representation from a single text by passing the text through the pre-trained network.
     """
-    #TODO: pass the pooling strategy as a command line option
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
     model = BertModel.from_pretrained('bert-base-uncased',
                                   output_hidden_states = True).to(DEVICE)
@@ -98,7 +109,7 @@ def bert_transform(single_text, pooling_strategy='cat'):
 def tfidf_transform(texts, **kwargs):
     return TFIDF_TRANSFORMER.transform(texts).todense()
 
-def taxonomy_transform(texts=None, urls=None):
+def taxonomy_transform(texts=[], urls=[]):
     if texts is None and urls is None:
         raise ValueError("Must provide either texts, urls or both.")
     return np.array([fe.ArticleVector(text=t, url=u).vector for t, u in zip(texts, urls)])
@@ -110,40 +121,62 @@ def bert_set(texts, pooling_strategy='cat', **kwargs):
 TRANSFORMS = dict(zip(REPRESENTATIONS, (bert_set, tfidf_transform, taxonomy_transform)))
 
 @click.command(no_args_is_help=True)
+
 @click.option('--single-text', '-s', default=None, type=click.STRING,
               help="""Classify a single string enclosed in double-quotes, provided in the command line. The result will be returned in the command line.
+              
+                      Example:
+                      
+                      python sfake.py --single-text \"Lorem ipsum dolor sit amet, consectetur adipiscing elit...\"""")
 
-                    Example:
-
-                    python sfake.py --single-text \"Lorem ipsum dolor sit amet, consectetur adipiscing elit...\"""")
 @click.option('--test-set', '-t',
               default=None,
               type=click.Path(dir_okay=False, resolve_path=True, allow_dash=False),
               help="""Path to a `.csv` file of labeled texts.
-              Its columns must contain:
-                    * str/int: labels 
+
+              Its first column should be an integer index.
+
+              Its other columns should contain:
+
+                    * str/int: labels
+
               and one or both of
+
                     * str: raw text
+
                     * str: the text's associated URL.
+
               One column should contain raw text, the other (which should be named 'label') string labels.""")
+
 @click.option('--model', '-m', default='sgd-taxonomy',
               type=click.Choice((os.path.basename(os.path.splitext(file)[0]) for file in glob.glob('models\\*[.pickle|.pkl]')),
                                 case_sensitive=True),
               help="Filename of model to use for classification, in the `models` directory, sans file extension.",
               show_choices=True)
-@click.option('--rep', '-r', default='taxonomy',
+
+@click.option('--rep', '-r', default='',
               type=click.Choice(REPRESENTATIONS),
               show_choices=True,
               help="Data representation you wish to use.")
-@click.option('--report', '-R', default=False, is_flag=True)
-@click.version_option('1.1.0', '--version', '-V', prog_name='SysFake CLI')
+
+@click.option('--report', '-R', default=False, is_flag=True,
+              help="""Use `sklearn.metrics.classification_report` to generate a detailed matrix of performance metrics by class.
+              
+                      WARNING: If a division by zero is encountered in metric calculation, the metric's value is assumed to be zero.""")
+
+@click.version_option('1.2.5',
+                      '--version', '-V',
+                      prog_name='SysFake CLI')
 def predict(model, rep, single_text, test_set, report):
     """
     A diverse CLI for using SysFake models to classify your own texts.
     """
-    #click.echo(f"[info] {model}, {rep}")
     if not single_text and not test_set:
-        raise click.BadParameter("Must specify `single_text` or `test_set`.",
+        raise click.BadParameter("Must specify `--single_text` or `--test_set`.",
+                                 param_hint=["--single-text", "--test-set"])
+    
+    if single_text and test_set:
+        raise click.BadParameter("`--single_text` and `--test_set` are mutually exclusive.",
                                  param_hint=["--single-text", "--test-set"])
     
     if not rep in model.lower():
@@ -152,20 +185,65 @@ def predict(model, rep, single_text, test_set, report):
                                  " representation and add that representation to its filename to suppress this warning.",
                                  param_hint=["--rep", "--model"])
 
+    # careful: if rep  is not provided as arg, it is INFERRED
+    # from the model filename and REASSIGNED.
+    if model and not rep:
+        try:
+            rep = next(name for name in REPRESENTATIONS if name in model.lower() and name)
+        except StopIteration:
+            raise click.BadParameter("Representation missing. Either specify using `--rep`"
+                                     " or put the representation in the model filename.",
+                                     param_hint=["--rep", "--model"])
+
     try:
         with open(f'models/{model}.pickle', mode='rb') as filein:
             model_obj = joblib.load(filein)
+
         click.echo(f"{model} loaded...")
+
     except:
         raise FileNotFoundError(f"{model} not found in the `models` directory")
 
     click.echo(f"Classifying {'single text' if single_text else 'new test set'} using {rep} data representation...")
 
-    test_set_obj = pd.read_csv(test_set) if test_set else [single_text]
-    transformed_text = TRANSFORMS[rep](texts=test_set_obj['text'] if test_set else test_set_obj, urls=test_set_obj['url'] if isinstance(test_set_obj, pd.DataFrame) and 'url' in test_set_obj.columns else None)
-    predictions = model_obj.predict(transformed_text)[0]
+    test_set_obj = pd.read_csv(test_set, index_col=0) if test_set else [single_text]
+
+    if isinstance(test_set_obj, pd.DataFrame):
+        test_set_obj.fillna('', inplace=True)
+
+        transformed_text = TRANSFORMS[rep](texts=test_set_obj['text'],
+                                           urls=test_set_obj['url'])
+
+        labels = test_set_obj['label']
+
+    else:
+        transformed_text = TRANSFORMS[rep](texts=test_set_obj, urls=['']).reshape(1,-1)
+
+    predictions = model_obj.predict(transformed_text)
+
     if single_text:
-        click.echo(f"Integer label: {predictions!s}, Class: {CLASS_DICT[predictions]}")
+        click.echo(f"Integer label: {predictions[0]!s}, Class: {CLASS_DICT[predictions[0]]}")
+    else:
+        if report:
+            report_string = classification_report(y_true=labels,
+                                                  y_pred=predictions,
+                                                  zero_division=0,
+                                                  labels=INT_TARGETS,
+                                                  target_names=STR_TARGETS)
+            click.echo(report_string)
+
+            pathlib.Path('./reports/').mkdir(parents=True, exist_ok=True)
+            with open("reports/"
+                      f"{os.path.basename(os.path.splitext(test_set)[0])}_{model}_"
+                      f"{dt.now().strftime('%m%d%H%M')}.txt", mode='w') as fileout:
+                fileout.write(report_string)
+
+        pathlib.Path('./predictions/').mkdir(parents=True, exist_ok=True)
+        test_set_obj['predicted'] = predictions
+        test_set_obj.to_csv("predictions/"
+                             f"{os.path.basename(os.path.splitext(test_set)[0])}_{model}_"
+                             f"{dt.now().strftime('%m%d%H%M')}.csv", index=False)
+        
 
     #if single_text:
     #    if rep=='taxonomy':
