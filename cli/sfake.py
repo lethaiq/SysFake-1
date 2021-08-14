9f2ddaab51b7852c14407c893bd1ac7fc8a222d0
Thai Le <tle01@verizonmedia.com>
2021-08-13 23:24:29 -0400

    update

--- cli/sfake.py
+++ cli/sfake.py
@@ -93,7 +93,7 @@ def bert_set(texts, pooling_strategy='cat', **kwargs):
 TRANSFORMS = dict(zip(REPRESENTATIONS, (bert_set, tfidf_transform, taxonomy_transform)))
 
 
-def predict(single_text, model='svc-tfidf', rep="tfidf", test_set=None, report=False):
+def predict(single_text, model='sgd-tfidf', rep="tfidf", test_set=None, report=False):
     with open(os.path.join(HERE, f'models/{model}.pickle'), mode='rb') as filein:
         model_obj = joblib.load(filein)
     print(f"{model} loaded...")
